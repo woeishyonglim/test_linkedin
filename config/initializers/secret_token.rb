dc75ae4f3a40c27a25b37ed8aecd7b1b15f429bd
woeishyonglim <woeishyonglim1989@gmail.com>
2015-03-02 15:03:18 +0800

    First commit -> Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestLinkedin::Application.config.secret_token = '049efe32cdacc77499405a7888d3e9ba58ce47ac8d1627d8210657360505f963c53203dc2e8850a260302e55c0038309f588556058ae1e33ec95ec0228ddf60d'
