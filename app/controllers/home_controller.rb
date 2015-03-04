fa414a45de1e0a6b5068c17a9e407167eeeee730
woeishyonglim <woeishyonglim1989@gmail.com>
2015-03-04 12:41:57 +0800

    Working version, display simple website with information from Linkedin account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
-    attr_accessor :x, :y
-    attr_accessor :counter
-
 
   # https://developer.linkedin.com/documents/profile-fields
   def submit_linkedin_resume
@@ -24,36 +21,79 @@ class HomeController < ApplicationController
     # Save it to session because need to reconstruct at the callback
     session[:rtoken] = rtoken
     session[:rsecret] = rsecret
-    # x = rtoken
-    # y = rsecret
-    # puts 'ni hao '
-    # puts x
-    # puts y
-    # puts session[:rtoken]
-    # puts 'helllo world'
     redirect_to request_token.authorize_url
   end
   
   def submit_linkedin_resume_cb
-      
+
       #redirect_to "http://www.google.com
       client = LinkedIn::Client.new(LINKEDIN_API_KEY, LINKEDIN_SECRET_KEY)
-      # if session[:rtoken].nil?
-      #puts 'hello friend'
-      #else
-      #puts 'ni hao ma, wo hen hao. ni ne? apa khabaar'
-      #end
       if session[:atoken].nil?
           pin = params[:oauth_verifier]
           atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
-          puts 'ni hao '
-          puts x
-          puts y
-          # atoken, asecret = client.authorize_from_request(x, y, pin)
           session[:atoken] = atoken
           session[:asecret] = asecret
-          else
+      else
           redirect_to "http://www.google.com"
       end
+      
+      profile = client.profile(fields: %w(
+                                           summary
+                                           email_address
+                                           first-name
+                                           last-name
+                                           date-of-birth
+                                           phone-numbers
+                                           main-address
+                                           positions
+                                           educations
+                                           location
+                                           industry
+                                           interests
+                                           languages
+                                           skills
+                                           specialties
+                                        ))
+      #TODO: Make it more efficient!
+      session[:summary] = profile.summary
+      session[:email] = profile.email_address
+      session[:first] = profile.first_name
+      session[:last] = profile.last_name
+      session[:birth] = profile.date_of_birth
+      session[:phone] = profile.phone_numbers
+      session[:address] = profile.main_address
+      session[:position] = profile.positions
+      session[:edu] = profile.educations
+      session[:location] = profile.location
+      session[:industry] = profile.industry
+      session[:interest] = profile.interests
+      session[:languages] = profile.languages
+      session[:skill] = profile.skills
+      session[:special] = profile.specialties
+      
+      session[:atoken] = nil
+      session[:asecret] = nil
+      redirect_to resume_sent_path
+
+   end
+  
+    def resume_sent
+        @summary = session[:summary]
+        @email = session[:email]
+        @first =session[:first]
+        @last = session[:last]
+        @birth = session[:birth]
+        @phone = session[:phone].phone_number
+        @address = session[:address]
+        @position = session[:position].title
+        @edu = session[:edu].degree
+        @location = session[:location].name
+        @industry = session[:industry]
+        @interest = session[:interest]
+        @languages = session[:languages]
+        @skill = session[:skill]
+        @special = session[:special]
+
     end
+  
 end
